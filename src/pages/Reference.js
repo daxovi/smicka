286e0e62d03b487380ac799d2c619d7881071dab
Dalibor Janecek <dalibor.janecek@gmail.com>
2024-06-20 14:27:33 +0000

    swa config react router

--- src/pages/Reference.js
+++ src/pages/Reference.js
@@ -4,10 +4,7 @@ import MDReader from '../components/MDReader'
 const Reference = () => {
   return (
     <div className="">
-    <MDReader file="ref-intro.md" />
-    <div className="ref">
-    <MDReader file="ref.md" />
-    </div>
+    Reference
     </div>
   )
 }
